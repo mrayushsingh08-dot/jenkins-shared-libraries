347e33b7a2b2baefa2a13d56d59ec7bfebd07219
Ayush singh <mrayushsingh08@gmail.com>
2026-09-23 13:36:36 +0530

    Update sonarqube_analysis.groovy

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -2,7 +2,7 @@ def call(String sonarServer, String projectKey, String projectName) {
     withSonarQubeEnv("${sonarServer}") {
         def scannerHome = tool 'sonar'
         sh """
-            ${scannerHome}/bin/sonar \
+            ${scannerHome}/bin/sonar-scanner \
                 -Dsonar.projectKey=${projectKey} \
                 -Dsonar.projectName=${projectName} \
                 -Dsonar.sources=.
