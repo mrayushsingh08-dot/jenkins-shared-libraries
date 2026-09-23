51c8ed2e6340f6d07e192196abb4b8a0c428b055
Ayush singh <mrayushsingh08@gmail.com>
2026-09-23 13:09:24 +0530

    Improve error handling in OWASP dependency check
    Refactor dependency check to handle errors and maintain build status.

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,16 +1,17 @@
 def call() {
-
     withCredentials([
         string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')
     ]) {
+        // catchError build status ko success/unstable rakhta hai taaki next stages block na hon
+        catchError(buildResult: 'SUCCESS', stageResult: 'UNSTABLE') {
+            dependencyCheck(
+                odcInstallation: 'OWASP',
+                additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY} --noupdate"
+            )
 
-        dependencyCheck(
-            odcInstallation: 'OWASP',
-            additionalArguments: "--scan ./ --nvdApiKey ${NVD_API_KEY}"
-        )
-
-        dependencyCheckPublisher(
-            pattern: '**/dependency-check-report.xml'
-        )
+            dependencyCheckPublisher(
+                pattern: '**/dependency-check-report.xml'
+            )
+        }
     }
 }
