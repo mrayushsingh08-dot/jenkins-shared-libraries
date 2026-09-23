400858b9e0ebb9076071aa637b286e5ac1a364cb
Ayush singh <mrayushsingh08@gmail.com>
2026-09-23 13:17:35 +0530

    Refactor SonarQube analysis function to accept parameters

--- vars/sonarqube_analysis.groovy
+++ vars/sonarqube_analysis.groovy
@@ -1,5 +1,11 @@
-def call() {
-    withSonarQubeEnv('sonarqube') {
-        sh 'mvn clean verify sonar:sonar'
+def call(String sonarServer, String projectKey, String projectName) {
+    withSonarQubeEnv("${sonarServer}") {
+        def scannerHome = tool 'sonar-scanner'
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+                -Dsonar.projectKey=${projectKey} \
+                -Dsonar.projectName=${projectName} \
+                -Dsonar.sources=.
+        """
     }
 }
