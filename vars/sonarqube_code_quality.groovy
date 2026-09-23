c26ad7944e8b5f302cad0e1501117633106f4bf5
Ayush singh <mrayushsingh08@gmail.com>
2026-09-23 14:13:09 +0530

    Increase SonarQube timeout to 10 minutes

--- vars/sonarqube_code_quality.groovy
+++ vars/sonarqube_code_quality.groovy
@@ -1,5 +1,13 @@
 def call() {
-    timeout(time: 2, unit: 'MINUTES') {
-        waitForQualityGate abortPipeline: false
+
+    timeout(time: 10, unit: 'MINUTES') {
+
+        def qualityGate = waitForQualityGate()
+
+        if (qualityGate.status != 'OK') {
+            error "SonarQube Quality Gate failed: ${qualityGate.status}"
+        }
+
+        echo "SonarQube Quality Gate passed: ${qualityGate.status}"
     }
 }
