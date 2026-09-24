e964aa1955004f42150d47f439f4c019aa410d4a
Ayush singh <mrayushsingh08@gmail.com>
2026-09-24 17:09:32 +0530

    Refactor OWASP dependency check script

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,17 +1,17 @@
 def call() {
 
-    sh '''
-        mkdir -p dependency-check-report
+    sh 'mkdir -p dependency-check-report'
 
-        dependency-check.sh \
-          --scan . \
-          --format XML \
-          --format HTML \
-          --out dependency-check-report \
-          --disableVersionCheck \
-          --project "Wanderlust"
-
-    '''
+    dependencyCheck(
+        odcInstallation: 'OWASP',
+        additionalArguments: '''
+            --scan .
+            --format XML
+            --format HTML
+            --out dependency-check-report
+            --disableVersionCheck
+        '''
+    )
 
     dependencyCheckPublisher(
         pattern: 'dependency-check-report/dependency-check-report.xml'
