fff09e95ecc8110997ce7dea77f5a1b51f31e18a
Ayush singh <mrayushsingh08@gmail.com>
2026-09-24 17:00:22 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,3 +1,17 @@
 def call() {
-    echo "OWASP Dependency-Check skipped temporarily - continuing pipeline..."
+
+    dependencyCheck(
+        odcInstallation: 'OWASP',
+        additionalArguments: '''
+            --scan .
+            --format XML
+            --format HTML
+            --out dependency-check-report
+            --disableOssIndex
+        '''
+    )
+
+    dependencyCheckPublisher(
+        pattern: 'dependency-check-report/dependency-check-report.xml'
+    )
 }
