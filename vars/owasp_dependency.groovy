68f3e1edba35cf3688b2dd6774ea8e06be6bce5f
Ayush singh <mrayushsingh08@gmail.com>
2026-09-24 17:07:41 +0530

    Update owasp_dependency.groovy

--- vars/owasp_dependency.groovy
+++ vars/owasp_dependency.groovy
@@ -1,15 +1,17 @@
 def call() {
 
-    dependencyCheck(
-        odcInstallation: 'OWASP',
-        additionalArguments: '''
-            --scan .
-            --format XML
-            --format HTML
-            --out dependency-check-report
-            --disableOssIndex
-        '''
-    )
+    sh '''
+        mkdir -p dependency-check-report
+
+        dependency-check.sh \
+          --scan . \
+          --format XML \
+          --format HTML \
+          --out dependency-check-report \
+          --disableVersionCheck \
+          --project "Wanderlust"
+
+    '''
 
     dependencyCheckPublisher(
         pattern: 'dependency-check-report/dependency-check-report.xml'
